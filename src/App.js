449cbf3b3bc7a47c99e3b6b756339811cfc98cde
olamiji-8 <olamijisotomi@gmail.com>
2023-05-12 12:32:56 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {Route, Routes} from "react-router-dom"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>App
+
+      <Routes>
+        <Route path='/taiwo'/>
+        <Route path='/kehinde'/>
+      </Routes>
+    </div>
   )
 }
 
