9940b71974f56ed197ab6fe299adc1eedd64fc8e
olamiji-8 <olamijisotomi@gmail.com>
2023-05-15 23:35:00 +0100

    update

---
+++ src/Cake.js
@@ -0,0 +1,12 @@
+import cake from "./cake.webp"
+import React from 'react'
+
+const Cake = () => {
+  return (
+    <div className="cake">
+        <img src={cake}  alt=""/>
+    </div>
+  )
+}
+
+export default Cake
