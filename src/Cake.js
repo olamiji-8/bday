ceb884a2ab775bf7f2acc0f882126052e07676cf
olamiji-8 <olamijisotomi@gmail.com>
2023-05-16 00:10:06 +0100

    update

--- src/Cake.js
+++ src/Cake.js
@@ -1,10 +1,11 @@
 import cake from "./cake.webp"
 import React from 'react'
+import "./components/header.css"
 
 const Cake = () => {
   return (
     <div className="cake">
-        <img src={cake}  alt=""/>
+        <img src={cake}  alt="" height="40px" style={{borderRadius:"50%"}}/>
     </div>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,13 @@ import React from 'react'
 import "./header.css"
 import Kehinde from '../pages/kehinde'
 import Tawio from '../pages/tawio'
+import Cake from '../Cake'
 
 const Header = () => {
   return (
 <header className="header">
       <nav className="navbar">
-        <div className="brand"></div>
+      <Cake/>
         <ul className="nav-items">
           <li className="nav-item">Tawio</li>
           <li className="nav-item">Kehinde</li>
